4797b18444a81694aa9addb47c8c2a11f6ebdabe
Bruno <bruno.noberto@hotmail.com>
2022-01-20 21:47:29 -0300

    adicionado codigo nos arquivos de constants e baseTest

--- src/test/java/core/BaseTest.java
+++ src/test/java/core/BaseTest.java
@@ -1,5 +1,28 @@
 package core;
 
-public class BaseTest {
+import org.hamcrest.Matchers;
+import org.junit.BeforeClass;
 
+import io.restassured.RestAssured;
+import io.restassured.builder.RequestSpecBuilder;
+import io.restassured.builder.ResponseSpecBuilder;
+
+public class BaseTest implements Constants {
+
+	@BeforeClass
+	public static void setup() {
+		RestAssured.baseURI = APP_BASE_URL;
+		RestAssured.port = APP_PORT;
+		
+		RequestSpecBuilder reqBuilder = new RequestSpecBuilder();
+		reqBuilder.setContentType(APP_CONTENT_TYPE);
+		RestAssured.requestSpecification = reqBuilder.build();
+		
+		ResponseSpecBuilder resBuilder = new ResponseSpecBuilder();
+		resBuilder.expectResponseTime(Matchers.lessThan(MAX_TIMEOUT));
+		resBuilder.expectStatusCode(200);
+		RestAssured.responseSpecification = resBuilder.build();
+		
+		RestAssured.enableLoggingOfRequestAndResponseIfValidationFails();
+	}
 }
--- src/test/java/core/Constants.java
+++ src/test/java/core/Constants.java
@@ -1,5 +1,13 @@
 package core;
 
+import io.restassured.http.ContentType;
+
 public interface Constants {
 
+	String APP_BASE_URL = "https://desafioonline.webmotors.com.br/api/OnlineChallenge";
+	Integer APP_PORT = 443;
+	
+	ContentType APP_CONTENT_TYPE = ContentType.JSON;
+	
+	Long MAX_TIMEOUT = 3000L;
 }
